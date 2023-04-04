034beb76cc418fbf2aab3c0bdf4a184b3af3480d
Jelly1500 <58739539+Jelly1500@users.noreply.github.com>
2023-04-05 00:09:09 +0900

    Update get_num_square.py

--- src/get_num_square.py
+++ src/get_num_square.py
@@ -5,7 +5,7 @@ if num:
   try:
      num = int(num)
   except Exception:
-     exit("ERROR: the INPUT_NUM provide("{}") is not an integer".format(num))
+     exit('ERROR: the INPUT_NUM provide("{}") is not an integer'.format(num))
 else:
    num = 1
 print(f"::set-output name=num_squared::{num ** 2}")
